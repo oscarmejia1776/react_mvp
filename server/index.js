67c06dac12cc2584e92133f07ddeea7bf712f79f
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-06-10 11:06:00 -0400

    Create and Read routes complete

--- server/index.js
+++ server/index.js
@@ -10,12 +10,44 @@ const app = express();
 
 app.use(express.json());
 
+///////////////////Initial Render/////////////////////
 app.get("/api/applications", (req, res) => {
   sql`SELECT * FROM applications`.then((rows) => {
     res.send(rows);
   });
 });
 
+///////////////////Creating New Application Card///////////////////////
+app.post("/api/applications", (req, res) => {
+  const {
+    company,
+    position,
+    submit_date,
+    response_date,
+    poc,
+    poc_email,
+    poc_phone,
+    status,
+    application_group,
+  } = req.body;
+
+  sql`
+    INSERT INTO applications (company, position, submit_date, response_date, poc, poc_email, poc_phone, status, application_group)
+    VALUES (${company}, ${position}, ${submit_date}, ${response_date}, ${poc}, ${poc_email}, ${poc_phone}, ${status}, ${application_group})
+    RETURNING *
+  `.then((result) => {
+    console.log("Result:", result);
+    if (!result || result.length === 0) {
+      // changed from !result.rows to !result
+      console.error("No rows returned from the insert operation");
+      res.sendStatus(500);
+      return;
+    }
+    res.status(201).send(result[0]); // changed from result.rows[0] to result[0]
+  });
+});
+
+//////////////Listening On Port/////////////////
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
